1020d52d88c8adcb59dfdf09e05564026cbda8ea
Christian Haas <christian.haas@sevensuns.at>
2026-07-18 10:03:28 +0200

    Move static linking of MinGW runtime libraries to toolchain files, where this belongs

--- cmake/toolchains/i686-w64-mingw32.cmake
+++ cmake/toolchains/i686-w64-mingw32.cmake
@@ -7,6 +7,9 @@ set(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
 set(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
+# Static runtime library linking
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static-libgcc -static-libstdc++")
+
 # where is the target environment
 set(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32 /usr/i686-w64-mingw32/sys-root/mingw)
 
