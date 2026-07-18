b8ed343eab15f929fd0a5159805794e4b3c89966
Christian Haas <christian.haas@sevensuns.at>
2026-07-18 11:20:46 +0200

    Fix wrong compilation target for Win32 (was still 64bit!), and clarify dependencies

--- cmake/toolchains/x86-64-w64-mingw32.cmake
+++ cmake/toolchains/x86-64-w64-mingw32.cmake
@@ -3,9 +3,13 @@ set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
 # specify the cross compiler
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc-posix)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++-posix)
 set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+
+set(CMAKE_C_FLAGS -m64)
+set(CMAKE_CXX_FLAGS -m64)
+
 
 # where is the target environment
 set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
@@ -19,3 +23,5 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 # CMake determines how to examine dependencies based on the *host* system, leading to
 # a `file unknown error` unless the target platform is explicitly specified.
 set(CMAKE_GET_RUNTIME_DEPENDENCIES_PLATFORM "windows+pe")
+
+set(TOOLCHAIN_DLL_DEPENDENCIES libgcc_s_seh-1.dll libstdc++-6.dll)
